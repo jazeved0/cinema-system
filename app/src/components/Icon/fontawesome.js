3707af862463ee22ca8f70e9b8bf0305c5e8791c
jazevedo620 <joseph.az@gatech.edu>
2019-11-17 16:50:14 -0500

    Remove unneeded icons

--- app/src/components/Icon/fontawesome.js
+++ app/src/components/Icon/fontawesome.js
@@ -3,12 +3,4 @@ export {
   faTimesCircle,
   faPlus
 } from "@fortawesome/free-solid-svg-icons";
-export {
-  faGithub,
-  faCcVisa,
-  faCcAmex,
-  faCcMastercard,
-  faCcJcb,
-  faCcDinersClub,
-  faCcDiscover
-} from "@fortawesome/free-brands-svg-icons";
+export { faGithub } from "@fortawesome/free-brands-svg-icons";
