d06d39182bd2ff159729615fa43b8a66de5244a3
jazevedo620 <joseph.az@gatech.edu>
2019-11-17 22:32:00 -0500

    Statically bake meta tags

--- app/src/Components/Page.jsx
+++ app/src/Components/Page.jsx
@@ -4,12 +4,6 @@ import { isDefined } from "Utility";
 import Helmet from "react-helmet";
 
 const siteTitle = "Cinema System";
-const description =
-  `This website is the result of a semester-long group project for CS 4400: ` +
-  `Intro Database Systems with Professor Mark Moss. In this project, we have` +
-  ` developed a movie system using relational database concepts and developed` +
-  ` an application layer/frontend with Python Flask and React.`;
-const card = "/card.png"
 export default function Page(props) {
   const { title, children } = props;
   const fullTitle = isDefined(title) ? `${title} | ${siteTitle}` : siteTitle;
@@ -21,22 +15,6 @@ export default function Page(props) {
           {
             property: "og:title",
             content: fullTitle
-          },
-          {
-            name: "description",
-            content: description
-          },
-          {
-            property: "og:description",
-            value: description
-          },
-          {
-            property: "og:site_name",
-            value: siteTitle
-          },
-          {
-            property: "og:image",
-            value: card
           }
         ]}
       />
