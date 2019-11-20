42d934b27646fba0de1b38373a90d8a8c71faaa4
jazevedo620 <joseph.az@gatech.edu>
2019-11-20 15:05:40 -0500

    Make brand an internal link

--- app/src/Components/Layout.jsx
+++ app/src/Components/Layout.jsx
@@ -14,7 +14,7 @@ export default function Layout({ children }) {
     <div className="layout">
       <Layout.DarkMode.Provider value={value}>
         <Navbar bg="primary" variant="dark" className="layout--nav">
-          <Navbar.Brand href="/" className="brand">
+          <Navbar.Brand as={Link} href="/" className="brand">
             <Icon name="film" className="brand--icon" />
             <h1 className="brand--text">Cinema System</h1>
           </Navbar.Brand>
