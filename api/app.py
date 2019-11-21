0a5a7a9c5e5c4267b274ee28787cb5ac467f5146
Joseph Azevedo <joseph.az@gatech.edu>
2019-11-20 19:24:00 -0500

    Add template routes/existence validator hoc

--- api/app.py
+++ api/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, redirect, request, g
 from flask_restful import Api, Resource, reqparse
 from flask_cors import CORS
+import functools
 import requests
 import json
 
@@ -9,20 +10,59 @@ app = Flask(__name__)
 cors = CORS(app)
 
 
+def route(params=()):
+    """
+    Wraps an app route with automatic parameter parsing and
+    existence validation
+    """
+
+    def _decorate(fn):
+        @functools.wraps(fn)
+        def wrapped(*args, **kwargs):
+            # Parse all parameters
+            parser = reqparse.RequestParser()
+            for param in params:
+                parser.add_argument(param)
+            param_values = parser.parse_args()
+
+            # Validate that all parameters were given
+            for param in params:
+                if param_values.get(param) is None:
+                    return "Malformed request", 400
+
+            # Call the function with the parameters
+            fn(*args, **param_values, **kwargs)
+        return wrapped
+    return _decorate
+
+
 @app.route('/login', methods=['POST'])
-def login():
-    parser = reqparse.RequestParser()
-    parser.add_argument('username')
-    parser.add_argument('password')
-    args = parser.parse_args()
-    if args.get('username') is None or args.get('password') is None:
-        return "Malformed request", 400
-
-    return "This should be a jwt but I haven't implemented that yet", 200
+@route(params=("username", "password"))
+def login(username, password):
+    return "not implemented", 400
 
 
-@app.route('/register', methods=['POST'])
-def register():
+@app.route('/register/user', methods=['POST'])
+@route(params=("first_name", "last_name", "username", "password"))
+def register_user(first_name, last_name, username, password):
+    return "not implemented", 400
+
+
+@app.route('/register/manager', methods=['POST'])
+@route(params=("first_name", "last_name", "username", "password", "company", "street_address", "city", "state", "zipcode"))
+def register_manager(first_name, last_name, username, password, company, street_address, city, state, zipcode):
+    return "not implemented", 400
+
+
+@app.route('/register/manager-customer', methods=['POST'])
+@route(params=("first_name", "last_name", "username", "password", "company", "street_address", "city", "state", "zipcode", "credit_cards"))
+def register_manager_customer(first_name, last_name, username, password, company, street_address, city, state, zipcode, credit_cards):
+    return "not implemented", 400
+
+
+@app.route('/register/customer', methods=['POST'])
+@route(params=("first_name", "last_name", "username", "password", "credit_cards"))
+def register_customer(first_name, last_name, username, password, credit_cards):
     return "not implemented", 400
 
 
