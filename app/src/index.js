c076cc2277b0367a084777c14db6dc0ced76fa72
jazevedo6 <jazevedo6@gatech.edu>
2019-11-30 11:47:10 -0500

    Unregister service worker due to cache issues

--- app/src/index.js
+++ app/src/index.js
@@ -5,4 +5,4 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-serviceWorker.register();
+serviceWorker.unregister();
